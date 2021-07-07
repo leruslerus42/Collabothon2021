f02b480349ca55a3f2c80d20bd101c59af1650e1
adamzaczek <adamzaczekul@gmail.com>
2021-07-07 21:57:19 +0200

    Points

--- src/routes/app/App.js
+++ src/routes/app/App.js
@@ -1,18 +1,27 @@
 import React from 'react'
-import { ThemeProvider } from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
 import BarCode from './BarCode'
 import theme from '../../themes'
 import GlobalStyles from './globalStyles'
 import Page from '../components/Page'
 import Points from './Points'
 
+const TopWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  background-color: yellowgreen;
+`
+
 const App = () => (
   <ThemeProvider theme={theme}>
     <Page>
       <GlobalStyles />
       {/* <Fetch /> */}
-      <Points value={1040} />
-      <BarCode value={123456789} size={300} />
+      <TopWrapper>
+        <Points value={1040} />
+        <BarCode value={123456789} size={300} />
+      </TopWrapper>
     </Page>
   </ThemeProvider>
 )
