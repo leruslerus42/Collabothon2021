5a95f9b16ab014d6d8b9a4e321cf6b4831603770
adamzaczek <adamzaczekul@gmail.com>
2021-07-07 21:53:26 +0200

    Points

---
+++ src/routes/app/Points.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components/macro'
+
+const Points = () => {
+  return (
+    <div>
+      <h2>Points</h2>
+      <div>1040</div>
+    </div>
+  )
+}
+
+export default Points
