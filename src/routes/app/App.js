ebc529601b092b22f4fafde4820c9fc05073a51d
adamzaczek <adamzaczekul@gmail.com>
2021-07-07 21:26:46 +0200

    QR code

--- src/routes/app/App.js
+++ src/routes/app/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
+import QRCode from 'react-qr-code'
 import theme from '../../themes'
 import GlobalStyles from './globalStyles'
 
@@ -7,6 +8,7 @@ const App = () => (
   <ThemeProvider theme={theme}>
     <GlobalStyles />
     <header className="App-header">Hello</header>
+    <QRCode value={123} size={300} />
   </ThemeProvider>
 )
 
