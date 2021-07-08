d848afda9ca6f908411687437da3a429648b48bc
adamzaczek <adamzaczekul@gmail.com>
2021-07-08 02:12:01 +0200

    Amplify

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom'
 import 'flexboxgrid/css/flexboxgrid.min.css'
 import Routes from './Routes'
 // import UserProvider from './providers/UserProvider'
+import { AmplifyAuthenticator } from '@aws-amplify/ui-react'
 
 import { ThemeProvider } from 'styled-components'
 import theme from './themes'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <Routes />
-    </ThemeProvider>
+    <AmplifyAuthenticator>
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
+    </AmplifyAuthenticator>
   </React.StrictMode>,
   document.getElementById('root')
 )
--- src/routes/app/BarCode.js
+++ src/routes/app/BarCode.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components/macro'
 import BarCode from 'react-barcode'
+
+import { Modal } from '@material-ui/core'
 
 const Wrapper = styled.div`
   display: flex;
@@ -8,15 +10,39 @@ const Wrapper = styled.div`
   justify-content: center;
   height: 400px;
   width: 400px;
+  cursor: pointer;
+`
+
+const ModalWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
 `
 
 /*
   Generated based on user id.
 */
-const QRCodeComponent = props => (
-  <Wrapper>
-    <BarCode {...props} />
-  </Wrapper>
-)
+const QRCodeComponent = props => {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <Wrapper>
+      <div onClick={() => setIsOpen(!isOpen)}>
+        <BarCode {...props} />
+      </div>
+      <Modal
+        onClick={() => setIsOpen(!isOpen)}
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <ModalWrapper>
+          <BarCode width={2} {...props} />
+        </ModalWrapper>
+      </Modal>
+    </Wrapper>
+  )
+}
 
 export default QRCodeComponent
